fb3b89482653fb5b8bc67553b4b7de155c2c1bc2
Stefano <b.fury90@gmail.com>
2018-05-28 22:12:33 +0200

    Comments to the code

--- View/View.h
+++ View/View.h
@@ -1,8 +1,21 @@
 #ifndef VIEW_VIEW_H_
 #define VIEW_VIEW_H_
-
+/**
+ * @brief Initialize the GUI
+ *
+ * It initialize all the Graphic objects in the GUI that either needs it or is
+ * more pratical to initialize its dinamically
+ */
 int view_init();
+
+/**
+ * @brief Looks at the actual Player outputs and represent its on the screen
+ */
 void view_update();
+
+/**
+ * @brief Destroy all objects that needs to be destroyed
+ */
 void view_exit();
 
 #endif /* VIEW_VIEW_H_ */
