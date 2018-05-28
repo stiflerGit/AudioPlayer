7e0f46d4e09057a284f44cd068090f0a4d0516ac
Stefano <b.fury90@gmail.com>
2018-05-28 22:12:14 +0200

    Comments to the code added. Fix last spectogram bar bug

--- View/View.c
+++ View/View.c
@@ -1,51 +1,50 @@
+/**
+ * @file	View.c
+ * @author	Stefano Fiori
+ * @date	26 May 2018
+ * @brief	Implementation of the View strictly linked to the Audio Player
+ * *
+ */
+#include "View.h"
 #include <stdio.h>
 #include <allegro.h>
-#include "View.h"
+#include <assert.h>
 #include "View_Config.h"
 #include "../Model/Player.h"
 
 #define handle_error(e) \
 	do{ perror(e); exit(EXIT_FAILURE);} while(1);
 
-
-/* ********* Spectogram structure **************/
 #define		MAXZOOM		5
-static const int ZOOM_TO_BAR[6] = { 227, 191, 155, 119, 83, 47};
+static const int ZOOM_TO_BAR[6] = { 210, 170, 140, 100, 70, 30};
+	/**< Zoom to bar table. */
 
+/**
+ * @brief	Structure for frequency spectogram panel
+ */
 struct fspect_panel_t{
-	Node 		bars[227];
-	unsigned char 	zoom;
+	Node 		bars[227];	/**< Bars of the spectogram. */
+	unsigned char 	zoom;		/**< Actual zoom of the panel.
+					It changes the no. bar shown. */
 }fspect_panel;
-/***********************************************/
 
 static Player	old_p;	/**< Previous player state. */
 
-static void print_f(float *buf, int size)
-{
-	printf("const float buf[%d] = {\n", size);
-	printf("\t%f", buf[0]);
-	for(int i = 1; i < size; i++){
-		printf("\t%f", buf[i]);
-		if(i % 5)
-			printf("\n");
-	}
-	printf("\n}\n");
-}
-
-/*
- * 	SPECTOGRAM PANEL
+/**
+ * @brief	Initialize variables of each frequency spectrum bar
+ *		that depends on zoom
  */
 static int fspect_panel_bars_init()
 {
-int	bar_x;
-int	bar_col;
-int	nbar;
-int	i;
+int	bar_x;		/**< X bars coordinate. */
+int	bar_col;	/**< Color of the bar. */
+int	nbar;		/**< No. bars. */
+int	i;		/**< Bars Array index. */
 
 	nbar = ZOOM_TO_BAR[fspect_panel.zoom];
 
 	bar_x = SPECT_X + 2;
-	bar_col = SPECT_COL;
+	bar_col = RED;
 
 	for (i = 0; i < nbar; i++) {
 		fspect_panel.bars[i] = (Node )
@@ -53,7 +52,7 @@ int	i;
 			bar_x, SPECT_Y + SPECT_H - 1, ((SPECT_W - 2) / nbar) - 2, 0,
 			bar_col, SPECT_BGCOL, 0, NULL};
 		bar_x += (SPECT_W - 2) / nbar;
-		bar_col = rand() % (0x80000000);
+		bar_col = RED;
 	}
 
 	g_draw(&(nodes[SPECT_PANEL][ZOOMIN_BTN]));
@@ -62,11 +61,19 @@ int	i;
 	return 0;
 }
 
-static float avg(float *v, int size)
+/**
+ * @brief	Compute the average value of a buffer.
+ * @param[in]	v	buffer of float falues.
+ * @param[in]	size	size of the buffer.
+ * @ret			average float value.
+ */
+static float avg(float *v, unsigned int size)
 {
 int	i;
 float	val;
 
+	if(v == NULL)
+		handle_error("NULL pointer");
 	val = 0;
 	for(i = 0; i < size; i++){
 		val += v[i];
@@ -75,32 +82,58 @@ float	val;
 	return val / ((float) size);
 }
 
-static void fspect_bar_update(int i)
+/**
+ * @brief	Draw a spectogram view bar with respect to the player spectogram
+ *
+ * Draw a bar of the spectogram depending on the player spectogram values corres
+ * -ponding to that bar. Indeed, each spectogram view bar represent more than a
+ * player spectogram value, since view bars are lesser.
+ *
+ * @param[in]	i	No bar to draw.
+ */
+ 
+static void fspect_bar_update(unsigned int i)
 {
-int	height;
-int	delta;
-int	col;
-int	nbar;
-Node	*n;
-
+int	height;	/**< New bar update. */
+int	delta;	/**< Difference between old height and new height of the bar. */
+int	col;	/**< Color with which to draw. */
+int	nbar;	/**< No. bar actually showed in the spect.. Depends on zoom. */
+Node	*n;	/**< Pointer to the right Spect. Bar. */
+	
 	nbar = ZOOM_TO_BAR[fspect_panel.zoom];
-	height = avg(&(p.spectogram[i * (PLAYER_WINDOW_SIZE_CPX / nbar)]),
+	assert(i<nbar);
+	// the last bar can overflow the player spectogram.
+	if (i < nbar){
+		height = 
+		avg(&(p.spectogram[i * (PLAYER_WINDOW_SIZE_CPX / nbar)]),
 		PLAYER_WINDOW_SIZE_CPX / nbar);
-	height = (SPECT_H * height) / 100;
-//	printf("bar_stretch(%d, -1, %d)\n",i, height);
+	} else {
+		height = 
+		avg(&(p.spectogram[i * (PLAYER_WINDOW_SIZE_CPX / nbar)]),
+		PLAYER_WINDOW_SIZE_CPX % nbar);
+	}
+	// since height is in the [0-100] range we can obtain easily the new
+	// height by multiplying for Panel Height
+	height = SPECT_H * height / 100;
 	n = &fspect_panel.bars[i];
+	// delta could be negative or positive.
 	delta = n->h - height;
-	if(delta == 0)
-		return;
+	// depending on delta sign, we have to clear or draw a rect.
 	col = (delta < 0) ? n->fg : n->bg;
-//	printf("rectfill(screen, %d, %d, %d, %d, %X)\n",n->x, n->y, n->x + n->    w, n->y + delta, col);
+	/*
+	printf("height: %d\t", height);
+	printf("delta: %d\t", delta);
+	printf("color: %d\n", col);
+	*/
 	scare_mouse();
 	rectfill(screen, n->x, n->y, n->x + n->w, n->y + delta, col);
+	unscare_mouse();
+	// if the bar draw on the buttons re-draws them
 	if(is_inside(&nodes[SPECT_PANEL][ZOOMIN_BTN], n->x, n->y))
 		g_draw(&nodes[SPECT_PANEL][ZOOMIN_BTN]);
 	if(is_inside(&nodes[SPECT_PANEL][ZOOMOUT_BTN], n->x, n->y))
 		g_draw(&nodes[SPECT_PANEL][ZOOMOUT_BTN]);
-	unscare_mouse();
+	// update bar state
 	n->y += delta;
 	n->h = height;
 }
@@ -127,156 +160,153 @@ int fspect_panel_zoomout()
 	return -1;
 }
 
-/*
- *	TITLE PANEL
+/*******************************************************************************
+ *			TITLE PANEL
+ ******************************************************************************/
+/**
+ * @brief	Draw the title in the title panel depending on player state.
+ *
+ * The title can be static or slide to the left or the right with different
+ * speed. It can also blink. It behaviour depends on the player state. This
+ * function update the title and draw it.
  */
-static void title_blink()
+static void title_draw()
 {
-int	col;
+char	slide[100];	/**< Buffer for title slide. */
+int 	len;		/**< title length. */
+int	speed;		/**< No. character to slide. */
+Node	*n;		/**< Pointer to a graphic Obj. */
+static int	blink_count = 0;
+			/**< Counter for title blinking. */
 
-	col = nodes[TITLE_PANEL][TITLE_TXT].fg;
-	nodes[TITLE_PANEL][TITLE_TXT].fg = nodes[TITLE_PANEL][TITLE_TXT].bg;
-	nodes[TITLE_PANEL][TITLE_TXT].bg = col;
-	g_draw(&nodes[TITLE_PANEL][TITLE_TXT]);
-}
+	n = &nodes[TITLE_PANEL][TITLE_TXT];
 
-static void title_reset()
-{
-text	*t;
+	if(p.state != PAUSE)
+		n->fg = WHITE;
 
-	t = (text *) nodes[TITLE_PANEL][TITLE_TXT].dp;
-	strcpy(t->str, p.trackname);
-	strcat(t->str, "       ");
-	g_draw(&nodes[TITLE_PANEL][TITLE_TXT]);
-}
-
-void title_slide(int speed)
-{
-Node	*t;
-char	slide[100];
-int	len;
-static	int	step = 0;
-
-	step = (step + 1) % 15;
-	if((step % 15) != 0)
-		return;
-	t = &nodes[TITLE_PANEL][TITLE_TXT];
-	len = strlen(((text *)(t->dp))->str);
-	strcpy(slide, &((text *)(t->dp))->str[(len - speed) % len]);
-	strncat(slide, ((text *)(t->dp))->str, (len - speed) % len);
-	slide[len] = '\0';
-	strcpy(((text *)(t->dp))->str, slide);
-	g_draw(t);
-}
-
-/*
- *	CONTROL PANEL
- */
-static void highlight(Node *n)
-{
-	if(n->fg == TSPRNT)
-		n->fg = RED;
-	else
-		n->fg = TSPRNT;
+	speed = 0;
+	switch (p.state) {
+	case STOP:
+		// reset title
+		strcpy(((text *)(n->dp))->str, p.trackname);
+		strcat(((text *)(n->dp))->str, "       ");
+		break;
+	case PLAY:
+		// title slide to the left
+		speed = 1;
+		break;
+	case PAUSE:
+		// title static blinking
+		blink_count = (blink_count + 1) % 5;
+		if(blink_count == 0)
+			n->fg = (n->fg == BLACK) ? WHITE : BLACK;
+		break;
+	case REWIND:
+		// title slide faster toward right
+		speed = -2;
+		break;
+	case FORWARD:
+		// title slide faster towards left
+		speed = 2;
+		break;
+	default:
+		break;
+	}
+	if (speed != 0) {
+		len = strlen(((text *)(n->dp))->str);
+		strcpy(slide, &((text *)(n->dp))->str[(len - speed) % len]);
+		strncat(slide, ((text *)(n->dp))->str, (len - speed) % len);
+		slide[len] = '\0';
+		strcpy(((text *)(n->dp))->str, slide);
+	}
 	g_draw(n);
 }
 
-static void draw_state()
+/*******************************************************************************
+ *			CONTROL PANEL
+ ******************************************************************************/
+/**
+ * @brief	Control Panel update
+ *
+ * Switch off the button corresponding to the older player state and than 
+ * hilight the button corresponding to the new palyer state
+ */
+static void control_draw()
 {
-static int	step = 0;
+Node	*n;
 
 	if(old_p.state != p.state){
+
 		switch (old_p.state) {
 		case STOP:
-			highlight(&nodes[CTRL_PANEL][STOP_BTN]);
+			n = (&nodes[CTRL_PANEL][STOP_BTN]);
 			break;
 		case PLAY:
-			highlight(&nodes[CTRL_PANEL][PLAY_BTN]);
+			n = (&nodes[CTRL_PANEL][PLAY_BTN]);
 			break;
 		case PAUSE:
-			highlight(&nodes[CTRL_PANEL][PAUSE_BTN]);
+			n = (&nodes[CTRL_PANEL][PAUSE_BTN]);
 			break;
 		case REWIND:
-			highlight(&nodes[CTRL_PANEL][RWND_BTN]);
+			n = (&nodes[CTRL_PANEL][RWND_BTN]);
 			break;
 		case FORWARD:
-			highlight(&nodes[CTRL_PANEL][FRWD_BTN]);
+			n = (&nodes[CTRL_PANEL][FRWD_BTN]);
 			break;
 		default:
 			break;
 		}
+		n->fg = TSPRNT;
+		g_draw(n);
 		switch (p.state) {
 		case STOP:
-			// stop button highlighted
-			highlight(&nodes[CTRL_PANEL][STOP_BTN]);
+			n = (&nodes[CTRL_PANEL][STOP_BTN]);
 			break;
 		case PLAY:
-			// play button highlighted
-			highlight(&nodes[CTRL_PANEL][PLAY_BTN]);
+			n = (&nodes[CTRL_PANEL][PLAY_BTN]);
 			break;
 		case PAUSE:
-			// pause button highlighted
-			highlight(&nodes[CTRL_PANEL][PAUSE_BTN]);
+			n = (&nodes[CTRL_PANEL][PAUSE_BTN]);
 			break;
 		case REWIND:
-			// rewind button highlighted
-			highlight(&nodes[CTRL_PANEL][RWND_BTN]);
+			n = (&nodes[CTRL_PANEL][RWND_BTN]);
 			break;
 		case FORWARD:
-			// forward button highlighted
-			highlight(&nodes[CTRL_PANEL][FRWD_BTN]);
+			n = (&nodes[CTRL_PANEL][FRWD_BTN]);
 			break;
 		default:
 			break;
 		}
+		n->fg = RED;
+		g_draw(n);
 	}
-	step = (step + 1) % 30;
-	switch (p.state) {
-		case STOP:
-			// title is static
-			title_reset();
-			break;
-		case PLAY:
-			// title slide to the left
-			if((step % 10) == 0)
-				title_slide(1);
-			break;
-		case PAUSE:
-			// title stopped blinking
-			if(step  == 0)
-				title_blink();
-			break;
-		case REWIND:
-			// title slide faster toward right
-			if((step % 10) == 0)
-				title_slide(-2);
-			break;
-		case FORWARD:
-			// title slide faster towards left
-			if((step % 10) == 0)
-				title_slide(2);
-			break;
-		default:
-			break;
 
-	}
 }
 
-/*
- *	VIEW
+/*******************************************************************************
+ *				VIEW
+ ******************************************************************************/
+/**
+ * @brief Initialize the GUI
+ *
+ * It call first the allegro functions that initialiaze a Windows on the screen
+ * Than draw all graphic objects defined statically in the configuration file.
+ * The only things that need to be initialized dinamically are the spectogram
+ * bars, but only for pratically reasons.
+ * Take the first copy of Player p.
  */
 int view_init()
 {
-int	i, j;
-Node	*t;
+int	i, j;	/**< Array indexes. */
+Node	*n;	/**< Pointer to a graphic Obj. */
 
 	set_color_depth(COL_D);
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, WIN_W, WIN_H, 0, 0);
 	clear_to_color(screen, 0);
 	
-	t = &nodes[TITLE_PANEL][TITLE_TXT];
-	strcpy(((text *)t->dp)->str, p.trackname);
-	strcat(((text *)t->dp)->str, "       ");
+	n = &nodes[TITLE_PANEL][TITLE_TXT];
+	strcpy(((text *)n->dp)->str, p.trackname);
+	strcat(((text *)n->dp)->str, "       ");
 
 	for(i = 0; i < NPANEL; i++){
 		for(j = 0; j < nodes_size[i]; j++){
@@ -284,11 +314,14 @@ Node	*t;
 		}
 	}
 
-	fspect_panel.zoom = 5;
+	fspect_panel.zoom = 4;
 	fspect_panel_bars_init();
 
 	memcpy(&old_p, &p, sizeof(Player));
-	highlight(&nodes[CTRL_PANEL][STOP_BTN]);
+
+	// What if the player already started to play??
+	nodes[CTRL_PANEL][STOP_BTN].fg = RED;
+	g_draw(&nodes[CTRL_PANEL][STOP_BTN]);
 
 	install_mouse();
 	show_mouse(screen);
@@ -296,18 +329,29 @@ Node	*t;
 	return 0;
 }
 
-
+/**
+ * @brief Looks at the actual Player outputs and represent its on the screen
+ *
+ * Compare the actual Player output and the last copy of player the View has,
+ * updated the last time view_update has been called.
+ * If something is changed it represents such a change and update its player 
+ * copy.
+ */
 void view_update()
 {
-int	i, j;
-int	nbv;
-int	spv;
-int	pix;
-Node	*n;
+int	i, j;	/**< Array indexes for spectogram. */
+int	nbv;	/**< No. bars of the View (Spectogram). */
+int	spv;	/**< player Spectogram bar Per View bar. */
+int	pix;	/**< Pixel variable. */
+Node	*n;	/**< Pointer to a graphic object. */
 
 	// PLAYER STATE
-	draw_state();
-	old_p.state = p.state;
+	if(old_p.state != p.state){
+		control_draw();
+		old_p.state = p.state;
+	}
+	// Title is always moving
+	title_draw();
 	// PLAYER TIME
 	if(old_p.time != p.time){
 		n = &nodes[POS_PANEL][POS_BAR];
@@ -322,17 +366,16 @@ Node	*n;
 		old_p.time = p.time;
 	}
 	// PLAYER SPECTOGRAM
+	/* DA RIVEDERE */
 	nbv = ZOOM_TO_BAR[fspect_panel.zoom];
 	spv = PLAYER_WINDOW_SIZE_CPX / nbv;
-	for(i = 0; i < PLAYER_WINDOW_SIZE_CPX; i++){
-		if(old_p.spectogram[i] != p.spectogram[i]){
-			j = i / spv;
-			fspect_bar_update(j);
-			if(j < nbv)
-				memcpy(&old_p.spectogram[j * spv], &p.spectogram[j * spv], spv);
-			else
-				memcpy(&old_p.spectogram[j * spv], &p.spectogram[j * spv], PLAYER_WINDOW_SIZE_CPX % nbv);
-			i = (j + 1) * spv;
+	for (i=0; i<nbv; i++){
+		for (j= i*spv; j < (i+1)*spv; j++){
+			if(old_p.spectogram[j] != p.spectogram[j]){
+				fspect_bar_update(i);
+				memcpy(&old_p.spectogram[j], &p.spectogram[j], 
+				sizeof(float)*(spv - (j % spv)));
+			}
 		}
 	}
 	// PLAYER VOLUME
