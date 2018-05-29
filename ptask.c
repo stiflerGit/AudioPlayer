f95b8ced7bf318918d920b3e2f4cdfd1a98adac6
Stefano <b.fury90@gmail.com>
2018-05-29 14:54:33 +0200

    Added comments to the code

--- ptask.c
+++ ptask.c
@@ -1,11 +1,27 @@
+/**
+ * @file	ptask.c
+ * @author	Stefano Fiori
+ * @date	28 May 2018
+ * @brief	
+ */
 #include "ptask.h"
 
+/**
+ * @brief	Copies a source time variable ts in a destination variable.
+ * @param[out]	td	pointer to the destination time variable
+ * @param[in]	ts	source time variable to copy
+ */
 static void time_copy(struct timespec *td, struct timespec ts)
 {
 	td->tv_sec = ts.tv_sec;
 	td->tv_nsec = ts.tv_nsec;
 }
 
+/**
+ * @brief	Adds a milliseconds value to a time variable.
+ * @param[out]	t	pointer to the time variable.
+ * @param[in]	ms	milliseconds value
+ */
 static void time_add_ms(struct timespec *t, int ms)
 {
 	t->tv_sec += ms/1000;
@@ -17,6 +33,12 @@ static void time_add_ms(struct timespec *t, int ms)
 	}
 }
 
+/**
+ * @brief	This function compares two time variables 
+ * @param[in]	t1	a time variable.
+ * @param[in]	t2	another time variable.
+ * @return	0 if they are equal, 1 if t1 > t2, ‐1 if t1 < t2.
+ */
 static int time_cmp(struct timespec t1, struct timespec t2)
 {
 	if (t1.tv_sec > t2.tv_sec) return 1;
@@ -26,7 +48,12 @@ static int time_cmp(struct timespec t1, struct timespec t2)
 	return 0;
 }
 
-
+/**
+ * @brief	Set the period for a periodic task.
+ *
+ * Reads the current time and computes the next activation time and the absolute 
+ * deadline of the task. NOTE: the timer is not set to interrupt.
+ */
 void set_period(struct task_par *tp)
 {
 struct timespec t;
@@ -38,6 +65,16 @@ struct timespec t;
 	time_add_ms(&(tp->dl), tp->deadline);
 }
 
+/**
+ * @brief	When called it controls if the task have a miss
+ *
+ * If the thread is still in execution when re‐activated, it increments the 
+ * value of dmiss.
+ *
+ * @param[in]	tp	task_par structure of the calling thread.
+ * @return 	If the thread is still in execution when re‐activated, it 
+ *		returns 1, otherwise returns 0.
+ */
 int deadline_miss(struct task_par *tp)
 {
 struct timespec now;
@@ -50,6 +87,16 @@ struct timespec now;
 	return 0;
 }
 
+/**
+ * @brief 	Suspends the calling thread until the next activation 
+ *
+ * Suspends the calling thread until the next activation and, when awaken, 
+ * updates activation time and deadline. 
+ * NOTE: Even though the thread calls time_add_ms() after the wake‐up time, the 
+ * computation is correct.
+ *
+ * @param[in]	tp	pointer to the task_param stucture of the calling thread
+ */
 void wait_for_period(struct task_par *tp)
 {
 	clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &(tp->at), NULL);
--- ptask.h
+++ ptask.h
@@ -1,21 +1,70 @@
+/**
+ * @file	ptask.h
+ * @author	Stefano Fiori
+ * @date	28 May 2018
+ * @brief	Provide a simple framework for Periodic Thread definitions
+ *
+ * The structure and the functions defined here serve as a framework for a peri-
+ * odic thread definition.
+ * Scheme of periodic thread:
+ *
+ * void *task(void *arg)
+ * {
+ * <local state variables>
+ * struct task_par	*tp;
+ *	// retrieves the pointer to task_par
+ *	tp = (struct task_par *)arg;
+ *	// retrieves the argument
+ *	i = tp->arg;
+ * 	set_period(tp);
+ *	while (1) {
+ *		<thread body>
+ *		if (deadline_miss(tp)) <do action>;
+ *		wait_for_period(tp);
+ *	}
+ * }
+ */
 #ifndef PTASK_H_
 #define PTASK_H_
 
 #include <time.h>
 
+/**
+ * @brief 	It provides all variable need for periodic tasks
+ *
+ * Such a structure must be initialized before calling the thread_create and 
+ * passed as a thread argument. After initialized can be passed to the functions
+ * defined in this library to obtain a periodic task behaviour.
+ * NOTE: A structure for each thread is required.
+ */
 struct task_par {
-	int		arg;		/**< Task argument.			*/
-	long int	wcet;		/**< Worst case execution time in us.	*/
-	int 		period;		/**< Period in ms.			*/
-	int 		deadline;	/**< Relative deadline in ms.		*/
-	int		priority;	/**< Task priority in [0; 99].		*/
-	int		dmiss;		/**< No. of deadline misses.		*/
-	struct timespec	at;		/**< next activation time.		*/
-	struct timespec dl;		/**< absolute deadline.			*/
+	int		arg;		/**< Task argument. */
+	long int	wcet;		/**< Worst case execution time in us. */
+	int 		period;		/**< Period in ms. */
+	int 		deadline;	/**< Relative deadline in ms. */
+	int		priority;	/**< Task priority in [0; 99]. */
+	int		dmiss;		/**< No. of deadline misses. */
+	struct timespec	at;		/**< next activation time. */
+	struct timespec dl;		/**< absolute deadline. */
 };
 
+/**
+ * @brief	set the period of a thread.
+ * @param[in]	tp	pointer to the task_par structure of the calling thread.
+ */
 void set_period(struct task_par *tp);
+
+/**
+ * @brief	check for a deadline miss.
+ * @param[in]	tp	pointer to the task_par structure of the calling thread.
+ * @ret		returns 1 in case of deadline miss, otherwise returns 0.
+ */
 int deadline_miss(struct task_par *tp);
+
+/**
+ * @brief	suspend the calling thread until the next activation.
+ * @param[in]	tp	pointer to the task_par structure of the calling thread.
+ */
 void wait_for_period(struct task_par *tp);
 
 #endif
